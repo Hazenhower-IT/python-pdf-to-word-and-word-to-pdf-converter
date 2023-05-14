8f93e637dc3f5a493316086a7d2c8a1e55ed1215
Hazenhower-IT <118206118+Hazenhower-IT@users.noreply.github.com>
2023-05-14 18:36:19 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from docx2pdf import convert
+
+convert("new.docx")
+
+# from pdf2docx import Converter
+
+# old_pdf = "abc.pdf"
+# new_doc = "new.docx"
+
+# obj = Converter(old_pdf)
+# obj.convert(new_doc)
+
+# obj.close()
+
